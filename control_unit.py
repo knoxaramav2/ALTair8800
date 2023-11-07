6fe1d878b844217349f5c41faef237bb9118a808
Woodrow Scott <woodrowtpscott@gmail.com>
2023-11-06 23:47:34 -0600

    Integrate memory fetch

--- control_unit.py
+++ control_unit.py
@@ -1,18 +1,19 @@
 
 
-from Shared import SharedCPU, SharedCU, SharedMem
+from Shared import ALU_Reg, SharedALU, SharedCPU, SharedCU, SharedMem
 from decoder import Decoder
 
 
 class ControlUnit(SharedCU):
 
-    __cpu       : SharedCPU
-    __mem       : SharedMem
-    __dec       : Decoder
-    __icc       : int
-    __mar       : int = 0
-    __mbr       : int = 0
-    __ir        : int = 0
+    __cpu           : SharedCPU
+    __alu           : SharedALU
+    __mem           : SharedMem
+    __dec           : Decoder
+    __icc           : int
+    __mar           : int = 0
+    __mbr           : int = 0
+    __ir            : int = 0
 
     def __mov_pc_mar(self):
          self.__mar = self.__cpu.inst_ptr
@@ -20,10 +21,10 @@ class ControlUnit(SharedCU):
     def __mov_mar_mbr(self):
          self.__mbr = self.__mem.get(self.__mar)
          inst_len = self.__dec.inst_len(self.__mbr)
-         if inst_len >= 1:
-              pass
-         if inst_len == 2:
-              pass
+         if inst_len >= 2:
+              self.__alu.set_reg(ALU_Reg.L, self.__mem.get(self.__mar+1))
+         if inst_len == 3:
+              self.__alu.set_reg(ALU_Reg.H, self.__mem.get(self.__mar+2))
          self.__cpu.next_addr(inst_len)
 
     def __mov_mbr_ir(self):
@@ -64,10 +65,11 @@ class ControlUnit(SharedCU):
          self.__icc = 0
          self.M1.set(True)
 
-    def __init__(self, scpu:SharedCPU, smem:SharedMem, dec:Decoder) -> None:
+    def __init__(self, scpu:SharedCPU, smem:SharedMem, sdec:Decoder, salu:SharedALU) -> None:
         super().__init__()
 
         self.__cpu = scpu
+        self.__alu = salu
         self.__mem = smem
-        self.__dec = dec
+        self.__dec = sdec
         self.__icc = 0
